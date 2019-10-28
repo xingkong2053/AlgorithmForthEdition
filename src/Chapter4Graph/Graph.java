c11c88c093b4aaffe641dc5257083710d00e75d9
xingkong2053 <lipenghui2053@gmail.com>
2019-10-28 21:53:24 +0800

    更新

---
+++ src/Chapter4Graph/Graph.java
@@ -0,0 +1,52 @@
+package Chapter4Graph;
+
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+
+public class Graph {
+
+    //顶点的数目
+    private final int V;
+    //边的数目
+    private int E;
+    //邻接表
+    private Bag<Integer>[] adj;
+
+    public Graph(int V){
+        this.V=V;this.E=0;
+        adj= ((Bag<Integer>[]) new Bag[V]);     //创建邻接表
+        for(int v=0;v<V;v++){
+            adj[v]=new Bag<>();                 //将所有的链表初始化
+        }
+    }
+
+    public Graph(In in){
+        this(in.readInt());             //读取V并将图初始化
+        int E=in.readInt();             //读取E
+        for (int i = 0; i < E; i++) {
+            int v=in.readInt();
+            int w=in.readInt();
+            addEdge(v,w);               //添加一条连接他们的边
+        }
+
+    }
+
+    public int V(){
+        return V;
+    }
+
+    public int E(){
+        return E;
+    }
+
+    public void addEdge(int v,int w){
+        adj[v].add(w);                      //将w添加到v的链表中
+        adj[w].add(v);                      //将v添加到w的链表中
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v){
+        return adj[v];
+    }
+
+}
